6dad19ef1e983a7284ac667e776fcb2b23321de6
Eduardo Felix <77497147+DuFelix@users.noreply.github.com>
2022-06-14 21:35:17 -0300

    Exportação do CSV

--- Scrapy.py
+++ Scrapy.py
@@ -1,7 +1,10 @@
 from email import header
+from sqlite3 import Row
 from wsgiref import headers
+from pyparsing import White
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url = 'https://cep.guiamais.com.br/busca/sao+paulo-sp?page=1'
 
@@ -9,3 +12,16 @@ headers = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) ApplewebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36"
 }
 
+site = requests.get(url, headers=headers)
+soup = BeautifulSoup(site.content, 'html.parser')
+placa = soup.find('table', class_='table s_table_box table-striped table-responsive')
+linha = placa.find_all('tr')
+ultima_pag = '1'
+
+find_table = soup.find('table') 
+rows = find_table.find_all('tr') 
+for i in rows: 
+    table_data = i.find_all('td') 
+    data = [j.text for j in table_data] 
+df = pd.DataFrame(data)
+df.to_csv('teste.csv')
